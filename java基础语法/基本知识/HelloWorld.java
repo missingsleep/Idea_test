e3a1553293e91587703ee467c3f5cbdfb23a8f46
missingsleep <zonghang_ok@foxmail.com>
2020-07-17 23:25:17 +0800

    更新到数据类型

--- java基础语法/基本知识/HelloWorld.java
+++ java基础语法/基本知识/HelloWorld.java
@@ -1,5 +1,14 @@
+/**
+	2020年7月17日 22:58:45
+	@author missingsleep
+	此处是文档注释
+**/
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("hello world");
     }
+	//单行注释
+	/*
+	多行注释
+	*/
 }
