51d993ae1f07ee96bddf5d54b9612ab625d2bba9
missingsleep <zonghang_ok@foxmail.com>
2020-07-18 23:34:34 +0800

    更新idea project，更新 Basic knowledge.md解读

---
+++ java_basic/Basic knowledge/javabasic01/src/com/basic_construct/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.basic_construct;
+
+/**
+ * @auther missingsleep
+ * @date2020/7/18 23:16
+ **/
+public class HelloWorld {
+    public  static  void main(String[] args){
+        System.out.println("hello world");
+    }
+}
